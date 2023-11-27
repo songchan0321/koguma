cd5ef2b51d09b02277b7cc299d49f0ac975a2f08
chan <dlscks97@gmail.com>
2023-11-27 11:31:16 +0900

    feat : MemberService 메서드 추가
    MemberService 메서드 추가, build.gradle Security 삭제
    
    2023.11.27 / 11:31 / SIC

--- src/main/java/com/fiveguys/koguma/data/dto/MemberDTO.java
+++ src/main/java/com/fiveguys/koguma/data/dto/MemberDTO.java
@@ -1,4 +1,27 @@
 package com.fiveguys.koguma.data.dto;
 
+import lombok.Data;
+
+import javax.management.relation.Role;
+
+@Data
 public class MemberDTO {
+    private String id;
+
+    private String nickname;
+    private String pw;
+    private String imageId;
+    private String phone;
+    private Float score;
+    private Boolean roleFlag;
+    private Boolean socialFlag;
+    private String email;
+    private String paymentAccount;
+    private String paymentBank;
+    private String paymentBalance;
+    private String paymentPw;
+    private Boolean activeFlag;
+
+
+
 }
