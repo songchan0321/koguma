4c3beae3eb874283d78effc714e1ab135f12638a
chan <dlscks97@gmail.com>
2023-11-28 20:17:50 +0900

    refactor : MemberServiceImpl 수정
    getMember 파라미터 MemberDTO > id로 변경
    
    2023.11.28 / 20:17 / SIC

--- src/main/java/com/fiveguys/koguma/data/dto/MemberRelationshipDTO.java
+++ src/main/java/com/fiveguys/koguma/data/dto/MemberRelationshipDTO.java
@@ -1,15 +1,15 @@
 package com.fiveguys.koguma.data.dto;
 
+import com.fiveguys.koguma.data.entity.MemberRelationship;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.util.Date;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 
 public class MemberRelationshipDTO {
 
@@ -18,6 +18,24 @@ public class MemberRelationshipDTO {
     public Long targetMemberId;
     public String content;
     public boolean type;
-    public Date regDate;
+    public LocalDateTime regDate;
+
+    public MemberRelationshipDTO() {
+
+    }
+
+    public static MemberRelationshipDTO fromEntity(MemberRelationship memberRelationship){
+        MemberRelationshipDTO memberRelationshipDTO = new MemberRelationshipDTO();
+        memberRelationshipDTO.setId(memberRelationship.getId());
+        memberRelationshipDTO.setSourceMemberId(memberRelationship.getSourceMemberId());
+        memberRelationshipDTO.setTargetMemberId(memberRelationship.getTargetMemberId());
+        memberRelationshipDTO.setContent(memberRelationship.getContent());
+        memberRelationshipDTO.setType(memberRelationship.getType());
+
+    }
+
+    public MemberRelationship toEntity(){
+        memberR
+    }
 
 }
