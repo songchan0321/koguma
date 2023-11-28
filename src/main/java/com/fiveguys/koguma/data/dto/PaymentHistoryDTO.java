b093df6487e05fe7ce13ba34bbda5da48859f737
git-kth <hisidblack@kakao.com>
2023-11-28 20:25:00 +0900

    temp: 임시 저장
    집가기 전 임시 저장, PaymentHistory Test 진행 중
    
    2023.11.28 20:25 KTH

--- src/main/java/com/fiveguys/koguma/data/dto/PaymentHistoryDTO.java
+++ src/main/java/com/fiveguys/koguma/data/dto/PaymentHistoryDTO.java
@@ -1,23 +1,48 @@
 package com.fiveguys.koguma.data.dto;
 
+import com.fiveguys.koguma.data.entity.Member;
 import com.fiveguys.koguma.data.entity.PaymentHistory;
 import com.fiveguys.koguma.data.entity.PaymentHistoryType;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
 public class PaymentHistoryDTO {
     private Long id;
     private PaymentHistoryType type;
     private Integer price;
     private String info;
-
+    private MemberDTO memberDTO;
+    private LocalDateTime regDate;
     @Builder
-    public PaymentHistoryDTO(PaymentHistoryType type, Integer price, String info) {
+    public PaymentHistoryDTO(PaymentHistoryType type, Integer price, String info, MemberDTO memberDTO) {
         this.type = type;
         this.price = price;
         this.info = info;
+        this.memberDTO = memberDTO;
+    }
+    public static PaymentHistoryDTO fromEntity(PaymentHistory paymentHistory) {
+        PaymentHistoryDTO paymentHistoryDTO = new PaymentHistoryDTO();
+        paymentHistoryDTO.setId(paymentHistory.getId());
+        paymentHistoryDTO.setPrice(paymentHistory.getPrice());
+        paymentHistoryDTO.setInfo(paymentHistory.getInfo());
+        paymentHistoryDTO.setType(paymentHistory.getType());
+        paymentHistoryDTO.setMemberDTO(MemberDTO.fromEntity(paymentHistory.getMember()));
+        paymentHistoryDTO.setRegDate(paymentHistory.getRegDate());
+        return paymentHistoryDTO;
+    }
+    public PaymentHistory toEntity() {
+        return PaymentHistory.builder()
+                .id(id)
+                .price(price)
+                .info(info)
+                .type(type)
+                .member(memberDTO.toEntity())
+                .build();
     }
 
-    public PaymentHistory toEntity() {
-        PaymentHistory.builder().member()
-    }
 }
