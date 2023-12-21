5de1bc63417d4cf1604898694df24eef8f272950
example <ax@example.com>
2023-12-21 21:29:35 +0900

    fix: clubcontroller 매개값 변경  추가  매개값 변경  사유: 리액트에서 clubId를 넘기는 것이 편함 / clubMember는 오류가 많음
    2023/12/21 11:36 ujh

---
+++ src/main/java/com/fiveguys/koguma/data/dto/club/ListClubByCategoryDTO.java
@@ -0,0 +1,95 @@
+package com.fiveguys.koguma.data.dto.club;
+
+import com.fiveguys.koguma.data.dto.CategoryDTO;
+import com.fiveguys.koguma.data.dto.ImageDTO;
+import com.fiveguys.koguma.data.entity.Club;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class ListClubByCategoryDTO {
+
+
+    private Long id;
+    private String title;
+    private String content;
+    private Integer maxCapacity;
+    private Double latitude;
+    private Double longitude;
+    private String dong;
+    private Boolean joinActiveFlag;
+    private Boolean activeFlag;
+    private String categoryName;
+    private CategoryDTO categoryDTO;
+    private List<ImageDTO> profileImage;
+
+    @Builder
+    public ListClubByCategoryDTO(Long id, String title, String content, Integer maxCapacity,
+                   Double latitude, Double longitude, String dong,
+                   Boolean joinActiveFlag, Boolean activeFlag,
+                   String categoryName, CategoryDTO categoryDTO, List<ImageDTO> profileImage){
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.maxCapacity = maxCapacity;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.dong = dong;
+        this.joinActiveFlag = joinActiveFlag;
+        this.activeFlag = activeFlag;
+        this.categoryName = categoryName;
+        this.categoryDTO = categoryDTO;
+        this.profileImage = profileImage;
+    }
+
+    public ListClubByCategoryDTO(Club club){
+        this.id = club.getId();
+        this.title = club.getTitle();
+        this.content = club.getContent();
+        this.maxCapacity = club.getMaxCapacity();
+        this.latitude = club.getLatitude();
+        this.longitude = club.getLongitude();
+        this.dong = club.getDong();
+        this.joinActiveFlag = club.isJoinActiveFlag();
+        this.activeFlag = club.isActiveFlag();
+        this.categoryName = club.getCategoryName();
+        this.categoryDTO = CategoryDTO.fromDTO(club.getCategory());
+    }
+
+    public Club toEntity(){
+        return Club.builder()
+                .id(this.id)
+                .title(this.title)
+                .content(this.content)
+                .maxCapacity(this.maxCapacity)
+                .latitude(this.latitude)
+                .longitude(this.longitude)
+                .dong(this.dong)
+                .joinActiveFlag(this.joinActiveFlag)
+                .activeFlag(this.activeFlag)
+                .categoryName(this.categoryName)
+                .category(categoryDTO.toEntity())
+                .build();
+    }
+
+    public static ListClubByCategoryDTO fromEntity(Club entity){
+        return ListClubByCategoryDTO.builder()
+                .id(entity.getId())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .maxCapacity(entity.getMaxCapacity())
+                .latitude(entity.getLatitude())
+                .longitude(entity.getLongitude())
+                .dong(entity.getDong())
+                .joinActiveFlag(entity.isJoinActiveFlag())
+                .activeFlag(entity.isActiveFlag())
+                .categoryName(entity.getCategoryName())
+                .categoryDTO(CategoryDTO.fromDTO(entity.getCategory()))
+                .profileImage(entity.getImages().stream().map(ImageDTO::fromEntity).collect(Collectors.toList()))
+                .build();
+    }
+
+}
